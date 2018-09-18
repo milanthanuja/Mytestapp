fd41f002cac6b78f85292baebf5e40e618cca613
milanthanuja <milanthanuja@gmail.com>
2018-09-18 05:32:22 +0000

    Mytestapp

---
+++ src/main/java/local/example/test/App.java
@@ -0,0 +1,15 @@
+package local.example.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World testing !!!!! This is successfull test " );
+
+
+    }
+}
