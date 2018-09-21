3e2f0d1bd397a5b452a2ffaf517f7e1bb178efb0
milanthanuja <milanthanuja@gmial.com>
2018-09-21 17:56:28 +0530

    Update App.java

--- src/main/java/local/example/test/App.java
+++ src/main/java/local/example/test/App.java
@@ -8,8 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World testing !!!!! This is successfull test " + args[0] );
+        
+        if (args.length > 0) {
+			System.out.println( "Hello World testing !!!!! This is successfull test " + args[0] );
+		} else {
+			System.out.println("Hello world!");
+		}
+        System.out.println( "Hello World testing"+System.getenv("NAME"));
 
-                         System.out.println( "Hello World testing"+System.getenv("NAME"));
+                         
     }
 }
